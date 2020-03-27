bc987c9c050eff33e998f5bc047ee133b6ec12b9
Vasuki Sunder <vasukisunder@Vasukis-MBP.attlocal.net>
2020-03-26 22:45:19 -0700

    completed assignment

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Form from './Form';
+import 'typeface-roboto';
+
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>User Onboarding</h1>
+      <Form />
+    </div>
+  );
+}
---
+++ user-onboarding/src/Form.js
@@ -0,0 +1,148 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import * as yup from "yup";
+import 'typeface-roboto';
+
+
+
+const formSchema = yup.object().shape({
+  name: yup.string().required("Name is a required field."),
+  email: yup
+    .string()
+    .email("Invalid email address.")
+    .required("Email is a required field."),
+  terms: yup.boolean().oneOf([true], "Please agree to terms and conditions"),
+  password: yup.string(),
+});
+
+export default function Form() {
+
+  const [buttonDisabled, setButtonDisabled] = useState(true);
+
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    password: "",
+    terms: "",
+  });
+
+  const [errors, setErrors] = useState({
+    name: "",
+    email: "",
+    password: "",
+    terms: "",
+  });
+
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    formSchema.isValid(formState).then(valid => {
+      setButtonDisabled(!valid);
+    });
+  }, [formState]);
+
+  const formSubmit = e => {
+    e.preventDefault();
+    axios
+      .post("https://reqres.in/api/users", formState)
+      .then(res => {
+        console.log(res.data);
+        setUsers(users.concat(res.data));
+
+        setFormState({
+          name: "",
+          email: "",
+          password: "",
+          terms: "",
+        });
+      })
+      .catch(err => console.log(err.response));
+  };
+
+  const validateChange = e => {
+    yup
+      .reach(formSchema, e.target.name)
+      .validate(e.target.value)
+      .then(valid => {
+        setErrors({
+          ...errors,
+          [e.target.name]: ""
+        });
+      })
+      .catch(err => {
+        setErrors({
+          ...errors,
+          [e.target.name]: err.errors[0]
+        });
+      });
+  };
+
+  const inputChange = e => {
+    e.persist();
+    const newFormData = {
+      ...formState,
+      [e.target.name]:
+        e.target.type === "checkbox" ? e.target.checked : e.target.value
+    };
+
+    validateChange(e);
+    setFormState(newFormData);
+  };
+  return (
+    <form onSubmit={formSubmit} >
+        
+      <label htmlFor="name">
+        Name 
+        <input
+          type="text"
+          name="name"
+          value={formState.name}
+          onChange={inputChange}
+        />
+        {errors.name.length > 0 ? <p className="error">{errors.name}</p> : null}
+      </label> 
+
+      <label htmlFor="email">
+        Email
+        <input
+          type="text"
+          name="email"
+          value={formState.email}
+          onChange={inputChange}
+        />
+        {errors.email.length > 0 ? (
+          <p className="error">{errors.email}</p>
+        ) : null}
+      </label> 
+
+      <label htmlFor="password">
+          Password
+        <input
+          type="password"
+          name="password"
+          value={formState.password}
+          onChange={inputChange}
+        />
+        {errors.password.length > 0 ? (
+          <p className="error">{errors.password}</p>
+        ) : null}
+      </label> 
+
+      <label htmlFor="terms" className="terms">
+        <input
+          type="checkbox"
+          name="terms"
+          checked={formState.terms}
+          onChange={inputChange}
+        />
+        Terms & Conditions
+      </label>
+
+      <button disabled={buttonDisabled}>Submit</button>
+
+      {/* displaying our post request data */}
+      <pre>{JSON.stringify(users, null, 2)}</pre>
+      
+    </form>
+  );
+}
